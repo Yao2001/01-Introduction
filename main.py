e009fc81b28b76673139974652c4e3d670bb0931
Yao2001 <xy31@iu.edu>
2019-08-31 00:03:46 +0800

    final  work

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ utils.check_version((3,7))
 utils.clear()
 
 print('Hello, Class!')
+
+input('What is your name?')
+print('\n My full name is Xihui Yao.')
+
+input('What is your favorite game?')
+print('\n League of Legends.')
+
+input('What concerns do you have about this class?')
+print('\n This is the first time I take a class like formally studying the code and how to write it. It is confused in the first place, but I hope it will be much more easier to understand after a few classes.')
+
+input('What are you excited about (in general)?')
+print('\n Having this class is a chanllenge for me. Learning more about what I am going to do in the future is quite excited.')
+
+input('What is your stackoverflow.com user number?')
+print('\n 11992174')
+
+input('What is the URL to your github.com profile?')
+print('\n github.com/Yao2001')
